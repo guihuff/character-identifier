1244e2c17a5ee4be74ca8b7b42843f8d27d464c8
Guilherme Feier Huff <ghuffg00@gmail.com>
2023-11-02 11:01:27 -0300

    images by path for character identifier

--- identifier-to-character.py
+++ identifier-to-character.py
@@ -1,27 +1,38 @@
 import cv2
 import pytesseract as pt
+import sys
 
-path = 'assets/'
+images = {}
 
-img = cv2.imread(path+'image2.png')
+for param in sys.argv[1:]:
+  path = param
+  img = cv2.imread(path)
+  if img is None:
+    print("a imagem não foi encontrada! (caminho: " + path + ")")
+    break;
 
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-threshold_value = 170
-_, binary = cv2.threshold(gray, threshold_value, 255, cv2.THRESH_BINARY)
+  gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+  threshold_value = 170
+  _, binary = cv2.threshold(gray, threshold_value, 255, cv2.THRESH_BINARY)
 
-boxes = pt.pytesseract.image_to_boxes(binary, lang='por')
+  boxes = pt.pytesseract.image_to_boxes(binary, lang='por')
 
-imH, imW, _ = img.shape
+  imH, imW, _ = img.shape
 
-print(boxes)
-
-for b in boxes.splitlines():
-  b = b.split(' ')
-  letra, x, y, w, h = b[0],int(b[1]),int(b[2]),int(b[3]),int(b[4])
-  if letra != '~' :
-    cv2.rectangle(img,(x,imH-y),(w,imH-h),(0,0,255),1)
-  # cv2.putText(img, letra, (x, imH-y+25),cv2.FONT_HERSHEY_SIMPLEX, 1, (255,0,0), 2)
+  for b in boxes.splitlines():
+    b = b.split(' ')
+    letra, x, y, w, h = b[0],int(b[1]),int(b[2]),int(b[3]),int(b[4])
+    if letra != '~' :
+      cv2.rectangle(img,(x,imH-y),(w,imH-h),(0,0,255),1)
+    # cv2.putText(img, letra, (x, imH-y+25),cv2.FONT_HERSHEY_SIMPLEX, 1, (255,0,0), 2)
 
 
-cv2.imshow('img', img)
+  images[path] = img
+  # cv2.imshow('Imagem', img)
+
+for key, image in images.items():
+    if image is not None:
+        cv2.imshow(key, image)
+
 cv2.waitKey(0)
+cv2.destroyAllWindows()
