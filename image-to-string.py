13ea121a8dbc652476bdca296829c3346d610255
Guilherme Feier Huff <ghuffg00@gmail.com>
2023-11-02 11:06:37 -0300

    image for param in image-to-string

--- image-to-string.py
+++ image-to-string.py
@@ -1,16 +1,26 @@
 import cv2
 import pytesseract as pt
 
-path = 'assets/'
+import sys
 
-img = cv2.imread(path+'image3.png')
+if len(sys.argv) > 2:
+  print("==============================")
+  print("somente um argumento é valido por requisição")
+  print("==============================\n\n\ncontinuando...\n")
 
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-threshold_value = 170
-_, binary = cv2.threshold(gray, threshold_value, 255, cv2.THRESH_BINARY)
+path = sys.argv[1]
 
-print(pt.pytesseract.image_to_string(binary, lang='por'))
+img = cv2.imread(path)
 
-# cv2.imshow('binary', binary)
-# cv2.waitKey(0)
+if img is None:
+  print("a imagem não foi encontrada! (caminho: " + path + ")")
+else:
+  gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+  threshold_value = 170
+  _, binary = cv2.threshold(gray, threshold_value, 255, cv2.THRESH_BINARY)
+
+  print(pt.pytesseract.image_to_string(binary, lang='por'))
+
+  # cv2.imshow('binary', binary)
+  # cv2.waitKey(0)
 
